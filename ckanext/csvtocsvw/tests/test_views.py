8026645738726a4def01f8dad85d0fd267f615df
Thomas Hanke <38633260+ThHanke@users.noreply.github.com>
2023-09-19 12:50:37 +0200

    Update test_views.py

--- ckanext/csvtocsvw/tests/test_views.py
+++ ckanext/csvtocsvw/tests/test_views.py
@@ -8,9 +8,9 @@ import pytest
 import ckan.plugins.toolkit as tk
 
 
-@pytest.mark.ckan_config("ckan.plugins", "csvtocsvw")
-@pytest.mark.usefixtures("with_plugins")
-def test_csvtocsvw_blueprint(app, reset_db):
-    resp = app.get(tk.h.url_for("csvtocsvw.page"))
-    assert resp.status_code == 200
-    assert resp.body == "Hello, csvtocsvw!"
+# @pytest.mark.ckan_config("ckan.plugins", "csvtocsvw")
+# @pytest.mark.usefixtures("with_plugins")
+# def test_csvtocsvw_blueprint(app, reset_db):
+#     resp = app.get(tk.h.url_for("csvtocsvw.page"))
+#     assert resp.status_code == 200
+#     assert resp.body == "Hello, csvtocsvw!"
